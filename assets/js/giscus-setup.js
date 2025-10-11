4df2fc875878c978f71401ab45329b39874d152a
kimh060612 <kimh060612@naver.com>
2025-10-11 17:07:23 +0000

    Deploying to gh-pages from @ kimh060612/cv@329cff704e7aa9a790223841ac54e4c3ab7b4038 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "kimh060612/michaelkim.github.io",
+    "data-repo": "kimh060612/cv",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
