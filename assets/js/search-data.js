81038bc47082c61135e3c9ccb10866dd5e23d7ea
kimh060612 <kimh060612@naver.com>
2025-10-12 09:17:05 +0000

    Deploying to gh-pages from @ kimh060612/cv@40c29c56d4f38b813f5e4c009eb7e3d77be3ac3a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/projects/";
-          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "",
